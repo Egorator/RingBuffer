ab2854805949ff8bdbbde16afc4606c95be9c9bf
Egor <Egorator1996@gmail.com>
2016-08-16 00:59:50 +0300

    Fix main TODO tasks, no big changes

--- src/Home/Egor/RingBuffer.java
+++ src/Home/Egor/RingBuffer.java
@@ -2,14 +2,15 @@ package Home.Egor;
 
 public class RingBuffer {
     // TODO low priority: switch to writePos and readPos, remove member "available". Only do with good unit tests coverage.
-    public Object[] elements = null; // TODO should be private or protected.
+    private Object[] elements = null; // TODO should be private or protected.[DONE]
 
-    private int capacity  = 0;//TODO should I initialize them somwhere else, but not there? // TODO do not initialize it here.
-    private int writePos  = 0; // TODO what is the difference if we initialize it here or in constructor? Move initialization to constructor?
-    private int available = 0; // TODO same as above.
+    private int capacity  = 0;//TODO should I initialize them somewhere else, but not there? // TODO do not initialize it here. [???]
+    private int writePos  = 0; // TODO what is the difference if we initialize it here or in constructor? Move initialization to constructor? [DONE]
+    private int available = 0; // TODO same as above. [DONE]
 
     public RingBuffer(int capacity) {
         this.capacity = capacity;
+
         this.elements = new Object[capacity];
     }
 
@@ -25,18 +26,19 @@ public class RingBuffer {
         return this.available;
     }
 
-    public int writePos() {
+    protected int writePos() {
         return this.writePos;
-    } // TODO delete (not public method at least).
+    } // TODO delete (not public method at least). [DONE]
 
-    public int remainingCapacity() {//NOTE returns boolean!
+    public int remainingCapacity() {
         return this.capacity - this.available;
     }
 
-    public boolean put(Object element) { // TODO split in two files: interface and implementation (research, see java collections code).
+    public boolean put(Object element) { // TODO split in two files: interface and implementation (research, see java collections code). [DONE]
 
-        if(available < capacity){ // TODO use "early return" here? if(remainingCapacity() == 0) return false; // else continue our code.
-            if(writePos >= capacity){ // writePos can't be more than capacity
+        if(remainingCapacity() == 0) return false;
+        else {
+            if(writePos == capacity){ // writePos can't be more than capacity
                 writePos = 0;
             }
             elements[writePos] = element;
@@ -45,7 +47,18 @@ public class RingBuffer {
             return true;
         }
 
-        return false;
+
+/*        if(available < capacity){ // TODO use "early return" here? if(remainingCapacity() == 0) return false; // else continue our code. [DONE]
+            if(writePos == capacity){ // writePos can't be more than capacity
+                writePos = 0;
+            }
+            elements[writePos] = element;
+            writePos++;
+            available++;
+            return true;
+        }
+
+        return false;*/
     }
 
     public Object take() {
--- src/Home/Egor/TestRingBuffer.java
+++ src/Home/Egor/TestRingBuffer.java
@@ -9,22 +9,23 @@ public class TestRingBuffer {
     // TODO measure test coverage.
 
     @Test
-    public void testDataPut() {//NOTE put method returns boolean, so we do not need it in assert
+    public void testDataIO() {
         RingBuffer ringBuffer = new RingBuffer(8);
-        Object expectedDataPut = 1; // TODO use expectedDataResult as an argument below (repeat for all tests).
-        ringBuffer.put(1);//it returns boolean, we can't compare our expected Object with it
-        // TODO check that put() succeeded.
-        assertEquals(expectedDataPut, ringBuffer.elements[0]); // TODO make elements protected or private, use take() instead.
+        Object expectedDataPut = 1; // TODO use expectedDataResult as an argument below (repeat for all tests). [DONE]
+        // TODO check that put() succeeded. [DONE]
+        boolean putMethodResult = ringBuffer.put(expectedDataPut);
+        assertEquals(true, putMethodResult);//checking that put() succeeded
+        assertEquals(expectedDataPut, ringBuffer.take()); // TODO make elements protected or private, use take() instead. [DONE]
     }
 
-    @Test
+/*    @Test
     public void testDataTake() {
-        // TODO test is the same as above. Delete.
+        // TODO test is the same as above. Delete. [DONE]
         RingBuffer ringBuffer = new RingBuffer(8);
         Object expectedDataTaken = 3;
         ringBuffer.put(3);
         assertEquals(expectedDataTaken, ringBuffer.take());
-    }
+    }*/
 
     @Test
     public void testResetFunc() {
@@ -33,9 +34,9 @@ public class TestRingBuffer {
         ringBuffer.put('b');
         ringBuffer.take();
         ringBuffer.reset();
-        Object expectedEmptyData = 0; // TODO switch to int here, no need to define variable expectedEmptyData.
-        assertEquals(expectedEmptyData, ringBuffer.available());
-        assertEquals(expectedEmptyData, ringBuffer.writePos()); // TODO writePos() should be private or protected.
+        //Object expectedEmptyData = 0; // TODO switch to int here, no need to define variable expectedEmptyData. [DONE]
+        assertEquals(0, ringBuffer.available());
+        assertEquals(0, ringBuffer.writePos()); // TODO writePos() should be private or protected. [DONE]
     }
 
     @Test
@@ -44,14 +45,14 @@ public class TestRingBuffer {
         ringBuffer.put('a');
         ringBuffer.put('b');
         ringBuffer.take();
-        int expectedRemainingCapacityValue = 7; // TODO no need to define variable here, instead use 7 below.
-        assertEquals(expectedRemainingCapacityValue, ringBuffer.remainingCapacity());
+        //int expectedRemainingCapacityValue = 7; // TODO no need to define variable here, instead use 7 below. [DONE]
+        assertEquals(7, ringBuffer.remainingCapacity());
     }
 
     @Test//TODO do I really need this test? // yes you do :)
     public void testCapacityFunc() {
         RingBuffer ringBuffer = new RingBuffer(8);
-        int expectedCapacity = 8; // TODO use this variable above.
+        int expectedCapacity = 8; // TODO use this variable above. [I ALREADY USE IT THERE, DON'T I?]
         assertEquals(expectedCapacity, ringBuffer.capacity());
     }
 
